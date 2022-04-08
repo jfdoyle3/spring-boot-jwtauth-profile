3be0f045f806969b37311ab9fbb78c2f4316129c
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2022-04-07 21:43:49 -0400

    added RestTemplate to webconfig

--- src/main/java/com/example/springjwtauth/security/WebSecurityConfig.java
+++ src/main/java/com/example/springjwtauth/security/WebSecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
@@ -51,6 +52,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
